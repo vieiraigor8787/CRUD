4f61ed6cee0d2f49fd975f7a75ab73cf305833c0
Igor Cerqueira <igor_vc@hotmail.com>
2019-08-12 21:47:20 -0300

    implementando rotas

--- OneDrive/Documentos/Curso_web_udemy_2019/web-moderno/react/cadastro-usuarios/frontend/src/main/App.jsx
+++ OneDrive/Documentos/Curso_web_udemy_2019/web-moderno/react/cadastro-usuarios/frontend/src/main/App.jsx
@@ -2,16 +2,19 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'font-awesome/css/font-awesome.min.css'
 import './App.css'
 import React from 'react'
+import {HashRouter} from 'react-router-dom'
+import Routes from './routes'
 
-import Home from '../components/home/Home'
 import Nav from '../components/templates/Nav'
 import Logo from '../components/templates/Logo'
 import Footer from '../components/templates/Footer'
 
 export default props =>
+    <HashRouter>
     <div className="app">
         <Logo />
         <Nav />
-        <Home />
+        <Routes />
         <Footer />
     </div>
+    </HashRouter>
