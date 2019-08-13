e35038dfc67fbdccd6659b5360d89698203950b8
Igor Cerqueira <igor_vc@hotmail.com>
2019-08-12 22:12:04 -0300

    funções de cadastro

--- OneDrive/Documentos/Curso_web_udemy_2019/web-moderno/react/cadastro-usuarios/frontend/src/components/user/UserCrud.jsx
+++ OneDrive/Documentos/Curso_web_udemy_2019/web-moderno/react/cadastro-usuarios/frontend/src/components/user/UserCrud.jsx
@@ -1,4 +1,6 @@
 import React, {Component} from 'react'
+import axios from 'axios'
+
 import Main from '../templates/Main'
 
 const headerProps = {
@@ -7,7 +9,36 @@ const headerProps = {
     subtitle: 'Cadastro de usuários: inclui, lista, altera e exclui'
 }
 
+const baseUrl = 'http://localhost:3001/users'
+const initiaState = {
+    user: { name: '', email: ''},
+    list: []
+}
+
 export default class UserCrud extends Component {
+
+    state = { ... initiaState}
+
+    clear(){
+        this.setState({ user: initiaState.user })
+    }
+    save(){
+        const user = this.state.user
+        const method = user.id ? 'put'/*true*/ : 'post' //false !!0 = false, !!1 = true, !!166 = true, !1000 = true
+        // se existir usuario será diferente de zero , ou seja put, senão será zero , sendo entao post
+        const url = user.id ? `${baseUrl}/${user.id}` /*se o id estiver setado*/ : baseUrl
+        axios[method](url,user)
+            .then(resp => {
+                const list = this.getUpdatedList(resp.data)
+                this.setState({ user: initiaState.user, list })
+            })
+    }
+    getUpdatedList(user){
+        const list = this.state.list.filter(u => u.id !== user.id)
+        list.unshift(user)
+        return list
+    }
+    
     render(){
         return (
             <Main {...headerProps}>
